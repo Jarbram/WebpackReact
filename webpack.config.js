757e3af30aafbb35e6ac8f0555d2d76a05214ca4
Abraham Huacchillo <huacchillocastillo@gmail.com>
2022-04-12 10:37:27 -0500

    configuracion de plugins y loaders para react

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,42 @@
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
+entry: './src/index.js',
+output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+},
+resolve: {
+    extensions: ['.js', '.jsx']
+},
+module: {
+    rules: [
+    {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+        loader: 'babel-loader',
+        }
     },
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use:{
-                    loader: 'babel-loader',
-                }
-            }
+    {
+        test: /\.html$/,
+        use: [
+        { loader: 'html-loader' }
         ]
-    },
-    devserver: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 3006,
     }
+    ]
+},
+plugins: [
+    new HtmlWebpackPlugin({
+    template: './public/index.html',
+    filename: './index.html'
+    })
+],
+    devServer: {
+        static: {
+        directory: path.join(__dirname, "dist/"),
+        },
+        port: 3006,
+},
 }
